92b94825cb8168b69770ddbb7206f9276cb20184
subroh0508 <subroh.0508@gmail.com>
2021-05-05 21:08:16 +0900

    Add .bashrc and link.sh

---
+++ link.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd ~/dotfiles
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".config" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    ln -s "$SCRIPT_DIR/$f" "$HOME/$f"
+    echo "$f"
+done
+
